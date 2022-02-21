021d10253bfa9db35c4841539d612613a822175c
Maclusky <x.mesones@gmail.com>
2022-02-21 23:07:42 +0100

    Oder Game

---
+++ Asteroides_v5 (recojer residuos de asteroides)/code/main.py
@@ -0,0 +1,158 @@
+import os.path
+import random
+import pygame, sys
+import pymunk
+from player import Player
+from rock import Rock
+from residuo import Residuo
+
+class Game():
+    def __init__(self):
+        self.FPS = 60
+        self.clock = pygame.time.Clock()
+
+        # Edges
+        self.screen_width = 600
+        self.screen_height = 600
+        self.space = pymunk.Space()
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.display.set_caption('Asteroides')
+
+        # Rocks (space, pos_x, pos_y, velocity, radius)
+        self.rocks = pygame.sprite.Group()
+        self.residuos = pygame.sprite.Group()
+        for _ in range(8):
+            velocity = (random.uniform(-60, 60), random.uniform(-200, 0))
+            self.rock_sprite = Rock(self.space, random.uniform(50, 550), self.screen_height + 20, velocity, 20)
+            self.rocks.add(self.rock_sprite)
+
+        #Player (pos, speed, constraint, images)
+        speed_player = 5
+        self.player_sprite = Player((self.screen_width/2, self.screen_height-10), speed_player, (0, self.screen_width, 0, self.screen_height), self.get_player_image())
+        self.player = pygame.sprite.GroupSingle(self.player_sprite)
+        # self.player_sprite.set_images(self.get_player_image_2())
+
+        # Fonts
+        self.consolas = pygame.font.match_font('consolas')
+        self.times = pygame.font.match_font('times')
+        self.couries = pygame.font.match_font('courier')
+
+        # Puntuacion
+        self.puntuacion = 0
+        self.num_residuos = 0
+
+    def update_nave(self):
+        if self.puntuacion >=  20 and self.puntuacion<40:
+            self.player_sprite.set_num_laser(2)
+        if self.puntuacion  >=  40:
+            self.player_sprite.set_num_laser(3)
+
+    def cc(self, point):
+        """
+            Concert coordinades
+        """
+        return int(point[0]), self.screen_height - int(point[1])
+
+    def get_player_image(self):
+        animacion_nave = []
+        for i in range(15):
+            archivo_nave = f'nave-1-{i:02d}.png'
+            image_raw = pygame.image.load(os.path.join('../graphics/ship/01', archivo_nave))
+            image = pygame.transform.scale(image_raw, (64, 32))
+            animacion_nave.append(image)
+        return animacion_nave
+
+    def get_player_image_2(self):
+        """
+            para demostrar que se puede cambiar na nave on the fly
+        """
+        animacion_nave = []
+        for i in range(16):
+            archivo_nave = f'nave-1-{i:02d}.png'
+            image = pygame.image.load(os.path.join('../graphics/ship/02', archivo_nave))
+            animacion_nave.append(image)
+        return animacion_nave
+
+    def collision_checks(self):
+        # player laser
+        if self.player.sprite.lasers:
+            for laser in self.player.sprite.lasers:
+                laser_rocks_colision = pygame.sprite.spritecollide(laser, self.rocks, False)
+                if laser_rocks_colision:
+                    for rock in laser_rocks_colision:
+                        print(rock.body.velocity)
+                        residuo_sprite = Residuo(rock.rect.x, rock.rect.y, 20, rock.body.velocity)
+                        self.residuos.add(residuo_sprite)
+                        rock.eliminar()
+                        self.puntuacion += 1
+                        velocity = (random.uniform(-60, 60), random.uniform(-200, 0))
+                        self.rock_sprite = Rock(self.space, random.uniform(50, 550), self.screen_height + 20, velocity, 20)
+                        self.rocks.add(self.rock_sprite)
+                        laser.kill()
+
+        # colision de rocas con player
+        player_rocks_collision = pygame.sprite.spritecollide(self.player_sprite, self.rocks, False)
+        if player_rocks_collision:
+            for rock in player_rocks_collision:
+                rock.eliminar()
+                velocity = (random.uniform(-60, 60), random.uniform(-200, 0))
+                self.rock_sprite = Rock(self.space, random.uniform(50, 550), self.screen_height + 20, velocity, 20)
+                self.rocks.add(self.rock_sprite)
+
+        # Captura de residuos
+        player_residuos_collision = pygame.sprite.spritecollide(self.player_sprite, self.residuos, False)
+        if player_residuos_collision:
+            for residuo in player_residuos_collision:
+                residuo.kill()
+                self.num_residuos += 1
+
+    def bounds(self):
+        for rock in self.rocks:
+            if rock.rect.x <= -50 or rock.rect.x >= 650 or rock.rect.y <= -50 or rock.rect.y >= 650 :
+                rock.eliminar()
+                velocity = (random.uniform(-60, 60), random.uniform(-200, 0))
+                self.rock_sprite = Rock(self.space, random.uniform(50, 550), self.screen_height + 20, velocity, 20)
+                self.rocks.add(self.rock_sprite)
+        for residuo in self.residuos:
+            if residuo.rect.x <= -50 or residuo.rect.x >= 650 or residuo.rect.y <= -50 or residuo.rect.y >= 650 :
+                residuo.kill()
+
+    def background(self):
+        self.screen.fill('grey')
+
+    def muestra_texto(self, fuente, texto, color, dimensiones, x, y):
+        tipo_letra = pygame.font.Font(fuente, dimensiones)
+        superficie = tipo_letra.render(texto, False, color)
+        rect = superficie.get_rect()
+        rect.center = (x, y)
+        self.screen.blit(superficie, rect)
+
+    def run(self):
+        self.background()
+        self.player.update()
+        self.update_nave()
+        self.player.sprite.lasers.draw(self.screen)
+        self.player.draw(self.screen)
+        self.rocks.update()
+        self.rocks.draw(self.screen)
+        self.residuos.update()
+        self.residuos.draw(self.screen)
+        self.bounds()
+        self.collision_checks()
+        self.space.step(1/self.FPS)
+        self.muestra_texto(self.consolas, str(self.puntuacion).zfill(4), 'red', 40, 550, 50)
+        self.muestra_texto(self.consolas, f'Residuos: {str(self.num_residuos).zfill(4)}', 'red', 40, 250, 50)
+        pygame.display.flip()
+        self.clock.tick(self.FPS)
+
+if __name__ == '__main__':
+    pygame.init()
+    game = Game()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        game.run()
+
---
+++ Asteroides_v5 (recojer residuos de asteroides)/code/player.py
@@ -0,0 +1,109 @@
+import random
+import pygame
+import pymunk
+from laser import Laser
+
+screen_width = 600
+screen_height = 600
+
+def cc(point):
+    return int(point[0]), screen_height-int(point[1])
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, speed, constraint, images):
+        super().__init__()
+        self.pos = pos
+        self.speed = speed
+        self.min_x_constraint = constraint[0]
+        self.max_x_constraint = constraint[1]
+        self.min_y_constraint = constraint[2]
+        self.max_y_constraint = constraint[3]
+
+        # Laser
+        self.laser_ready = True
+        self.laser_time = 0
+        self.laser_cooldown = 400
+        self.laser_speed = 8
+        self.lasers = pygame.sprite.Group()
+
+        # reload player
+        self.set_images(images)
+
+        self.num_laser = 1
+
+    def set_images(self, images):
+        self.max_fotograms = len(images)
+        self.image = images[self.max_fotograms-1]
+        self.image_recharge = images
+        self.rect = self.image.get_rect(midbottom=self.pos)
+        self.time_fotogram_zero = 0
+        self.fotogram = 0
+        self.time_fotogram = self.laser_cooldown / self.max_fotograms
+
+    def recharge(self):
+        if not self.laser_ready:
+            current_time = pygame.time.get_ticks()
+
+            if self.time_fotogram_zero == 0:
+                self.image = self.image_recharge[self.fotogram]
+                self.time_fotogram_zero = current_time # 1000
+            else:
+                if current_time - self.time_fotogram_zero >= self.time_fotogram and self.fotogram < self.max_fotograms:
+                    self.fotogram += 1
+                    self.image = self.image_recharge[self.fotogram]
+                    self.time_fotogram_zero = current_time
+
+            if (current_time - self.laser_time) >= self.laser_cooldown:
+                self.laser_ready = True
+                self.fotogram = 0
+                self.time_fotogram_zero = 0
+                self.image = self.image_recharge[self.max_fotograms-1]
+
+    def set_num_laser(self, num):
+        self.num_laser = num
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+        # movement of the player
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.speed
+        # movement of the laser
+        if keys[pygame.K_SPACE] and self.laser_ready:
+            if self.num_laser == 1:
+                self.shoot_laser(self.rect.center)
+            elif self.num_laser == 2:
+                self.shoot_laser(self.rect.midright)
+                self.shoot_laser(self.rect.midleft)
+            elif self.num_laser == 3:
+                self.shoot_laser((self.rect.centerx, self.rect.centery - 10))
+                self.shoot_laser(self.rect.midright)
+                self.shoot_laser(self.rect.midleft)
+
+            self.laser_ready = False
+            self.laser_time = pygame.time.get_ticks()
+
+    def constraint(self):
+        if self.rect.right >= self.max_x_constraint - 10:
+            self.rect.right = self.max_x_constraint - 10
+        if self.rect.left <= self.min_x_constraint + 10:
+            self.rect.left = self.min_x_constraint + 10
+        if self.rect.top <= self.min_y_constraint + 20:
+            self.rect.top = self.min_y_constraint + 20
+        if self.rect.bottom >= self.max_y_constraint - 20:
+            self.rect.bottom = self.max_y_constraint - 20
+
+    def shoot_laser(self, pos):
+        laser = Laser(pos, self.laser_speed)
+        self.lasers.add(laser)
+
+    def update(self):
+        self.get_input()
+        self.constraint()
+        self.recharge()
+        self.lasers.update()
